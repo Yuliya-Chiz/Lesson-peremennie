87eda5d794459bfcdea6d498e1e7d41d83019f7f
Yuliya-Chiz <yuliiamngwb@gmail.com>
2024-09-19 00:24:24 +1000

    Add files via upload

---
+++ Lesson_peremennie.py
@@ -0,0 +1,5 @@
+dz = "12"
+chas = "1.5"
+name_kurs = "Python"
+vremya = (float(chas)/int(dz))
+print("Курс:",name_kurs,",", "всего задач:",dz,", ","затрачено часов:", chas, "среднее время выполнения ",vremya, "часа.")
